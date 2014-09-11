41caa1cd8ea3d96d5a5ec9a22684502fd2ff0fc5
todoespacoonline <todoespacoonline@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-11 01:55:39 +0000

    Added modal style option
    git-svn-id: https://plugins.svn.wordpress.org/tutsup-simple-modal/trunk@986882 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tutsup-modal.php
+++ tutsup-modal.php
@@ -3,7 +3,7 @@
 Plugin Name: Tutsup Modal
 Plugin URI: http://www.tutsup.com/
 Description: This plugin allows you to create simple modals in your site/blog. You can put any custom HTML and CSS inside your modal, like subscription forms, warnings, advertisement, simple text, or whatever you want. It's really simple.
-Version: 0.2.1
+Version: 0.2.2
 Author: Luiz Otávio Miranda
 Author URI: http://www.tutsup.com/
 License: GPLv2 or later
@@ -56,9 +56,9 @@ if ( ! class_exists('TutsupModalSettings') ) {
 			} 
 			
 			// Isn't admin
-			else {
-				// Plugin styles and scripts
-				add_action( 'wp_footer', array( $this, 'enqueue_public_scripts' ), 1000 );
+			else {				
+				// Plugin front-end HTML
+				add_action( 'wp_footer', array( $this, 'load_plugin_html' ), 100000 );
 				
 			}
 		}
@@ -89,6 +89,7 @@ if ( ! class_exists('TutsupModalSettings') ) {
 		public function register_mysettings() {
 			//register our settings
 			register_setting( 'tutsup_modal', 'tutsup_modal' );
+			register_setting( 'tutsup_modal_style', 'tutsup_modal_style' );
 		}
 		
 		/**
@@ -100,7 +101,8 @@ if ( ! class_exists('TutsupModalSettings') ) {
 			 */
 			function tab_settings( $current = 'theme' ) {
 				$tabs = array(
-					'tutsup-index'  => __('Main', 'tutsup'),
+					'tutsup-index'        => __('Main', 'tutsup'),
+					'tutsup-modal-style'  => __('Modal Style', 'tutsup'),
 				);
 				
 				echo '<div id="icon-themes" class="icon32"><br></div>';
@@ -134,115 +136,20 @@ if ( ! class_exists('TutsupModalSettings') ) {
 		}
 		
 		/**
-		 * Loads the styles and scripts (public)
+		 * Loads plugin front-end HTML
 		 */
-		public function enqueue_public_scripts() {			
+		public function load_plugin_html() {			
 			$tutsup_option = get_option('tutsup_modal');
+			$tutsup_style = get_option('tutsup_modal_style');
 			
-?>
+			wp_enqueue_script('jquery');
+			
+			// Include the plugin HTML
+			if ( tutsup_check_option( $tutsup_option, 'tutsup-modal-html' ) ) {
+				require dirname( __FILE__ ) . '/views/tutsup-modal-html.php';
+			}
 
-<?php if ( tutsup_check_option( $tutsup_option, 'tutsup-modal-html' ) ): ?>
-
-<style>
-.tutsup-modal-bg {
-	width: 100%;
-	height: 100%;
-	position: fixed;
-	top: 0;
-	left: 0;
-	background: rgba(0,0,0,.6);
-	z-index: 999;
-	display: none;
-}
-.tutsup-modal {
-	width: 500px;
-	height: 300px;
-	position: fixed;
-	top: 50%;
-	left: 50%;
-	margin-top: -150px;
-	margin-left: -250px;
-	background: #fff;
-	z-index: 1000;
-	box-shadow: 0 0 10px #000;
-	display: none;
-}
-.tutsup-modal-inner{
-	position: relative;
-}
-.tutsup-modal-content{
-	margin: 20px;
-	overflow: auto;
-	height: 260px;
-}
-.tutsup-modal-close {
-	display: block;
-	font-size: 12px;
-	line-height: 12px;
-	padding: 5px;
-	border-radius: 50%;
-	background: #000;
-	color: #fff;
-	text-align: center;
-	position: absolute;
-	top: -30px;
-	right: -13px;
-	font-weight: 700;
-	font-family: sans-serif;
-	z-index: 1001;
-	cursor: pointer;
-}
-<?php echo tutsup_check_option( $tutsup_option, 'tutsup-modal-css' );?>
-</style>
-
-<div class="tutsup-modal-bg"></div>
-<div class="tutsup-modal">
-	<div class="tutsup-modal-inner">
-		<div class="tutsup-modal-close">&#10005;</div>
-		<div class="tutsup-modal-content">
-			<?php echo tutsup_check_option( $tutsup_option, 'tutsup-modal-html' );?>
-		</div>
-	</div>
-</div>
-
-<script>
-function tutsup_modal_init($) {
-	var tutsup_modal_cookies = document.cookie;
-	
-	if ( tutsup_modal_cookies.indexOf( 'tutsup-modal' ) == -1 ) {
-		$('.tutsup-modal-bg').css('display', 'block');
-		$('.tutsup-modal').css('display', 'block');
-		document.cookie = 'tutsup-modal=1';
-	}
-}
-
-jQuery(function($){
-	$('.tutsup-modal-close').click(function(){
-		$('.tutsup-modal-bg').css('display', 'none');
-		$('.tutsup-modal').css('display', 'none');
-		document.cookie = 'tutsup-modal=1';
-	});
-
-	<?php 
-	$modal_seconds = tutsup_check_option( $tutsup_option, 'tutsup-modal-seconds' );
-	
-	if ( empty( $modal_seconds ) ): 
-	?>
-		$('body').mouseleave(function() {
-			tutsup_modal_init($);
-		});
-	<?php else: ?>
-		setTimeout(function(){
-			tutsup_modal_init($);
-		}, <?php echo ( (int)tutsup_check_option( $tutsup_option, 'tutsup-modal-seconds' ) ) * 1000; ?>);
-	<?php endif; ?>
-});
-</script>
-
-<?php endif; ?>
-
-<?php
-		} // enqueue_public_scripts
+		} // load_plugin_html
 	}
 
 	/* Loads the class */
